02f9b0b2e0018174efd22a91c17ec4bee4b68dd8
Theoness1 <sasha.bolotskih2002@gmail.com>
2022-05-22 02:39:10 +0700

    ес

---
+++ src/main/java/ru/theone_ss/vanilla_claws/item/material/VanillaClawsMaterial.java
@@ -0,0 +1,74 @@
+package ru.theone_ss.vanilla_claws.item.material;
+
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.tag.ItemTags;
+import net.minecraft.util.Lazy;
+
+import java.util.function.Supplier;
+
+public enum VanillaClawsMaterial implements ToolMaterial {
+
+    WOOD(0, 79, 2.0F, 0.0F, 1, () -> {
+        return Ingredient.fromTag(ItemTags.PLANKS);
+    }),
+    STONE(1, 161, 4.0F, 0.0F, 1, () -> {
+        return Ingredient.fromTag(ItemTags.STONE_TOOL_MATERIALS);
+    }),
+    COPPER(1, 231, 2.0F, 0.0F, 1, () -> {
+        return Ingredient.ofItems(Items.COPPER_INGOT);
+    }),
+    IRON(2, 320, 6.0F, 0.0F, 1, () -> {
+        return Ingredient.ofItems(Items.IRON_INGOT);
+    }),
+    DIAMOND(3, 761, 8.0F, 2.0F, 1, () -> {
+        return Ingredient.ofItems(Items.DIAMOND);
+    }),
+    GOLD(0, 62, 12.0F, 0.0F, 100, () -> {
+        return Ingredient.ofItems(Items.GOLD_INGOT);
+    }),
+    NETHERITE(0, 1131, 9.0F, 2.0F, 1, () -> {
+        return Ingredient.ofItems(Items.NETHERITE_INGOT);
+    });
+
+    private final int miningLevel;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Lazy<Ingredient> repairIngredient;
+
+    VanillaClawsMaterial(int miningLevel, int itemDurability, float miningSpeed, float attackDamage, int enchantability, Supplier<Ingredient> repairIngredient) {
+        this.miningLevel = miningLevel;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairIngredient = new Lazy(repairIngredient);
+    }
+
+    public int getDurability() {
+        return this.itemDurability;
+    }
+
+    public float getMiningSpeedMultiplier() {
+        return this.miningSpeed;
+    }
+
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    public int getMiningLevel() {
+        return this.miningLevel;
+    }
+
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    public Ingredient getRepairIngredient() {
+        return (Ingredient)this.repairIngredient.get();
+    }
+}
--- src/main/java/ru/theone_ss/vanilla_claws/registry/VanillaClawsItems.java
+++ src/main/java/ru/theone_ss/vanilla_claws/registry/VanillaClawsItems.java
@@ -3,10 +3,10 @@ package ru.theone_ss.vanilla_claws.registry;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ToolMaterials;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import ru.theone_ss.vanilla_claws.item.ClawsItem;
+import ru.theone_ss.vanilla_claws.item.material.VanillaClawsMaterial;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -15,13 +15,13 @@ public class VanillaClawsItems {
 
     public static final Map<Identifier, Item> ITEMS = new LinkedHashMap<>();
 
-    public static final Item NETHERITE_CLAWS = add("netherite_claws", new ClawsItem(ToolMaterials.NETHERITE, 3, 3, settings()));
-    public static final Item DIAMOND_CLAWS = add("diamond_claws", new ClawsItem(ToolMaterials.DIAMOND, 3, 3, settings()));
-    public static final Item IRON_CLAWS = add("iron_claws", new ClawsItem(ToolMaterials.IRON, 3, 3, settings()));
-    public static final Item GOLDEN_CLAWS = add("golden_claws", new ClawsItem(ToolMaterials.GOLD, 3, 3, settings()));
-    public static final Item COPPER_CLAWS = add("copper_claws", new ClawsItem(ToolMaterials.STONE, 3, 3, settings()));
-    public static final Item STONE_CLAWS = add("stone_claws", new ClawsItem(ToolMaterials.STONE, 3, 3, settings()));
-    public static final Item WOODEN_CLAWS = add("wooden_claws", new ClawsItem(ToolMaterials.WOOD, 3, 3, settings()));
+    public static final Item NETHERITE_CLAWS = add("netherite_claws", new ClawsItem(VanillaClawsMaterial.NETHERITE, 4, 2, settings()));
+    public static final Item DIAMOND_CLAWS = add("diamond_claws", new ClawsItem(VanillaClawsMaterial.DIAMOND, 3, 2, settings()));
+    public static final Item IRON_CLAWS = add("iron_claws", new ClawsItem(VanillaClawsMaterial.IRON, 4, 2, settings()));
+    public static final Item GOLDEN_CLAWS = add("golden_claws", new ClawsItem(VanillaClawsMaterial.GOLD, 3, 2, settings()));
+    public static final Item COPPER_CLAWS = add("copper_claws", new ClawsItem(VanillaClawsMaterial.COPPER, 2, 2, settings()));
+    public static final Item STONE_CLAWS = add("stone_claws", new ClawsItem(VanillaClawsMaterial.STONE, 3, 2, settings()));
+    public static final Item WOODEN_CLAWS = add("wooden_claws", new ClawsItem(VanillaClawsMaterial.WOOD, 2, 2, settings()));
 
     private static Item add(String name, Item item) {
         ITEMS.put(new Identifier("vanilla_claws", name), item);
