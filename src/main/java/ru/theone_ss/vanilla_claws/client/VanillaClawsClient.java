5e75e373142b5d31adc589f77ff9d94dc3ab3c17
Theoness1 <sasha.bolotskih2002@gmail.com>
2023-09-13 17:29:17 +0700

    Identifier problem fix

--- src/main/java/ru/theone_ss/vanilla_claws/client/VanillaClawsClient.java
+++ src/main/java/ru/theone_ss/vanilla_claws/client/VanillaClawsClient.java
@@ -3,12 +3,11 @@ package ru.theone_ss.vanilla_claws.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
 import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.util.Identifier;
 import ru.theone_ss.vanilla_claws.item.ClawsItem;
 import ru.theone_ss.vanilla_claws.registry.VanillaClawsItems;
 
 public class VanillaClawsClient implements ClientModInitializer {
-
-
 
     @Override
     public void onInitializeClient() {
@@ -18,9 +17,8 @@ public class VanillaClawsClient implements ClientModInitializer {
         });
 
         ModelLoadingRegistry.INSTANCE.registerModelProvider((manager, out) -> TwoModelsItemRegistry.ENTRIES.forEach((id, item) ->
-                out.accept(new ModelIdentifier(id + "_in_hand#inventory"))
+                out.accept(new ModelIdentifier(new Identifier(id + "_in_hand"), "inventory"))
         ));
     }
-
  }
 
